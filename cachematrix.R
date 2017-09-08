9c16f9fe61576dd6c1d32d2f724a9e33ae9b8e29
eedekobi <30658245+eedekobi@users.noreply.github.com>
2017-09-08 09:58:36 +0200

    Add files via upload

---
+++ cachematrix.R
@@ -0,0 +1,77 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+}
+
+
+
+## A pair of functions that are used to create a matrix, caches it, and returns the inverse.
+## Makes sense caching the value, and looked up when needed, rather than computing it again and again.
+
+## The function makeCacheMatrix() creates a matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+    mat <- NULL
+    set <- function(y) {
+        x <<- y
+        mat <<- NULL
+    }
+    get <- function() x
+    setmatrix <- function(matrix) mat <<- matrix
+    getmatrix <- function() mat
+    list(set = set,
+         get = get,
+         setmatrix = setmatrix,
+         getmatrix = getmatrix)
+}
+
+## Caching the Inverse of the Matrix returned above.
+## If the is already cached, it retrieves the inverse from cache.
+
+cacheSolve <- function(x, ...) {
+    ## Returns x matrix inverse
+    mat <- x$getmatrix()
+    if (!is.null(mat)) {
+        message("getting cached data")
+        return(mat)
+    }
+    data <- x$get()
+    mat <- solve(data, ...)
+    x$setmatrix(mat)
+    mat
+}
+
+
+
+
+# ----------- Below is the Functions test:
+
+
+> x <- makeCacheMatrix(matrix(c(3, 1, 4, 2), 2, 2))
+> x$get()
+     [,1] [,2]
+[1,]    3    4
+[2,]    1    2
+> x$getmatrix()
+NULL
+> cacheSolve(x)
+     [,1] [,2]
+[1,]  1.0 -2.0
+[2,] -0.5  1.5
+> cacheSolve(x)
+getting cached data
+     [,1] [,2]
+[1,]  1.0 -2.0
+[2,] -0.5  1.5
+> 
